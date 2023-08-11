66bb6a8bb64a1015d55708e3f041c5aa36abfc8b
aimeesimons <smnaim002@myuct.ac.za>
2023-08-11 21:00:57 +0200

    Last batch of serial

--- ParallelAssignment2023/main.sh
+++ ParallelAssignment2023/main.sh
@@ -1,38 +1,179 @@
 #! /usr/bin/bash
 
-for i in {1..5};do make runSerial ARGS="5050 5050 0 505000 0 505000 0.5";done
+for i in {1..5};do make runSerial ARGS="5610 5610 0 56100 0 56100 0.05";done
+for i in {1..5};do make runSerial ARGS="5610 5610 0 56100 0 56100 0.25";done
+for i in {1..5};do make runSerial ARGS="5610 5610 0 56100 0 56100 0.5";done
 
 
-for i in {1..5};do make runSerial ARGS="5120 5120 0 51200 0 51200 0.05";done
-for i in {1..5};do make runSerial ARGS="5120 5120 0 51200 0 51200 0.25";done
-for i in {1..5};do make runSerial ARGS="5120 5120 0 51200 0 51200 0.5";done
+for i in {1..5};do make runSerial ARGS="5680 5680 0 56800 0 56800 0.05";done
+for i in {1..5};do make runSerial ARGS="5680 5680 0 56800 0 56800 0.25";done
+for i in {1..5};do make runSerial ARGS="5680 5680 0 56800 0 56800 0.5";done
 
 
-for i in {1..5};do make runSerial ARGS="5190 5190 0 51900 0 51900 0.05";done
-for i in {1..5};do make runSerial ARGS="5190 5190 0 51900 0 51900 0.25";done
-for i in {1..5};do make runSerial ARGS="5190 5190 0 51900 0 51900 0.5";done
+for i in {1..5};do make runSerial ARGS="5750 5750 0 575000 0 575000 0.05";done
+for i in {1..5};do make runSerial ARGS="5750 5750 0 575000 0 575000 0.25";done
+for i in {1..5};do make runSerial ARGS="5750 5750 0 575000 0 575000 0.5";done
 
 
-for i in {1..5};do make runSerial ARGS="5260 5260 0 55400 0 55400 0.05";done
-for i in {1..5};do make runSerial ARGS="5260 5260 0 55400 0 55400 0.25";done
-for i in {1..5};do make runSerial ARGS="5260 5260 0 55400 0 55400 0.5";done
+for i in {1..5};do make runSerial ARGS="5820 5820 0 58200 0 58200 0.05";done
+for i in {1..5};do make runSerial ARGS="5820 5820 0 58200 0 58200 0.25";done
+for i in {1..5};do make runSerial ARGS="5820 5820 0 58200 0 58200 0.5";done
 
 
-for i in {1..5};do make runSerial ARGS="5330 5330 0 53300 0 53300 0.05";done
-for i in {1..5};do make runSerial ARGS="5330 5330 0 53300 0 53300 0.25";done
-for i in {1..5};do make runSerial ARGS="5330 5330 0 53300 0 53300 0.5";done
+for i in {1..5};do make runSerial ARGS="5890 5890 0 58900 0 58900 0.05";done
+for i in {1..5};do make runSerial ARGS="5890 5890 0 58900 0 58900 0.25";done
+for i in {1..5};do make runSerial ARGS="5890 5890 0 58900 0 58900 0.5";done
 
 
-for i in {1..5};do make runSerial ARGS="5400 5400 0 54000 0 54000 0.05";done
-for i in {1..5};do make runSerial ARGS="5400 5400 0 54000 0 54000 0.25";done
-for i in {1..5};do make runSerial ARGS="5400 5400 0 5400 0 5400 0.5";done
+for i in {1..5};do make runSerial ARGS="5960 5960 0 59600 0 59600 0.05";done
+for i in {1..5};do make runSerial ARGS="5960 5960 0 59600 0 59600 0.25";done
+for i in {1..5};do make runSerial ARGS="5960 5960 0 59600 0 59600 0.5";done
 
 
-for i in {1..5};do make runSerial ARGS="5470 5470 0 54700 0 54700 0.05";done
-for i in {1..5};do make runSerial ARGS="5470 5470 0 54700 0 54700 0.25";done
-for i in {1..5};do make runSerial ARGS="5470 5470 0 54700 0 54700 0.5";done
+for i in {1..5};do make runSerial ARGS="6030 6030 0 60300 0 60300 0.05";done
+for i in {1..5};do make runSerial ARGS="6030 6030 0 60300 0 60300 0.25";done
+for i in {1..5};do make runSerial ARGS="6030 6030 0 60300 0 60300 0.5";done
 
 
-for i in {1..5};do make runSerial ARGS="5540 5540 0 55400 0 55400 0.05";done
-for i in {1..5};do make runSerial ARGS="5540 5540 0 55400 0 55400 0.25";done
-for i in {1..5};do make runSerial ARGS="5540 5540 0 55400 0 55400 0.5";done
+for i in {1..5};do make runSerial ARGS="6100 6100 0 61000 0 61000 0.05";done
+for i in {1..5};do make runSerial ARGS="6100 6100 0 61000 0 61000 0.25";done
+for i in {1..5};do make runSerial ARGS="6100 6100 0 61000 0 61000 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6170 6170 0 61700 0 61700 0.05";done
+for i in {1..5};do make runSerial ARGS="6170 6170 0 61700 0 61700 0.25";done
+for i in {1..5};do make runSerial ARGS="6170 6170 0 61700 0 61700 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6240 6240 0 62400 0 62400 0.05";done
+for i in {1..5};do make runSerial ARGS="6240 6240 0 62400 0 62400 0.25";done
+for i in {1..5};do make runSerial ARGS="6240 6240 0 62400 0 62400 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6310 6310 0 63100 0 63100 0.05";done
+for i in {1..5};do make runSerial ARGS="6310 6310 0 63100 0 63100 0.25";done
+for i in {1..5};do make runSerial ARGS="6310 6310 0 63100 0 63100 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6380 6380 0 63800 0 63800 0.05";done
+for i in {1..5};do make runSerial ARGS="6380 6380 0 63800 0 63800 0.25";done
+for i in {1..5};do make runSerial ARGS="6380 6380 0 63800 0 63800 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6450 6450 0 645000 0 645000 0.05";done
+for i in {1..5};do make runSerial ARGS="6450 6450 0 645000 0 645000 0.25";done
+for i in {1..5};do make runSerial ARGS="6450 6450 0 645000 0 645000 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6520 6520 0 65200 0 65200 0.05";done
+for i in {1..5};do make runSerial ARGS="6520 6520 0 65200 0 65200 0.25";done
+for i in {1..5};do make runSerial ARGS="6520 6520 0 65200 0 65200 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6590 6590 0 65900 0 65900 0.05";done
+for i in {1..5};do make runSerial ARGS="6590 6590 0 65900 0 65900 0.25";done
+for i in {1..5};do make runSerial ARGS="6590 6590 0 65900 0 65900 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6660 6660 0 66600 0 66600 0.05";done
+for i in {1..5};do make runSerial ARGS="6660 6660 0 66600 0 66600 0.25";done
+for i in {1..5};do make runSerial ARGS="6660 6660 0 66600 0 66600 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6730 6730 0 67300 0 67300 0.05";done
+for i in {1..5};do make runSerial ARGS="6730 6730 0 67300 0 67300 0.25";done
+for i in {1..5};do make runSerial ARGS="6730 6730 0 67300 0 67300 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6800 6800 0 68000 0 68000 0.05";done
+for i in {1..5};do make runSerial ARGS="6800 6800 0 68000 0 68000 0.25";done
+for i in {1..5};do make runSerial ARGS="6800 6800 0 6800 0 6800 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6870 6870 0 68700 0 68700 0.05";done
+for i in {1..5};do make runSerial ARGS="6870 6870 0 68700 0 68700 0.25";done
+for i in {1..5};do make runSerial ARGS="6870 6870 0 68700 0 68700 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="6940 6940 0 69400 0 69400 0.05";done
+for i in {1..5};do make runSerial ARGS="6940 6940 0 69400 0 69400 0.25";done
+for i in {1..5};do make runSerial ARGS="6940 6940 0 69400 0 69400 0.5";done
+
+for i in {1..5};do make runSerial ARGS="7010 7010 0 70100 0 70100 0.05";done
+for i in {1..5};do make runSerial ARGS="7010 7010 0 70100 0 70100 0.25";done
+for i in {1..5};do make runSerial ARGS="7010 7010 0 70100 0 70100 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7080 7080 0 70800 0 70800 0.05";done
+for i in {1..5};do make runSerial ARGS="7080 7080 0 70800 0 70800 0.25";done
+for i in {1..5};do make runSerial ARGS="7080 7080 0 70800 0 70800 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7150 7150 0 715000 0 715000 0.05";done
+for i in {1..5};do make runSerial ARGS="7150 7150 0 715000 0 715000 0.25";done
+for i in {1..5};do make runSerial ARGS="7150 7150 0 715000 0 715000 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7220 7220 0 72200 0 72200 0.05";done
+for i in {1..5};do make runSerial ARGS="7220 7220 0 72200 0 72200 0.25";done
+for i in {1..5};do make runSerial ARGS="7220 7220 0 72200 0 72200 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7290 7290 0 72900 0 72900 0.05";done
+for i in {1..5};do make runSerial ARGS="7290 7290 0 72900 0 72900 0.25";done
+for i in {1..5};do make runSerial ARGS="7290 7290 0 72900 0 72900 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7360 7360 0 73600 0 73600 0.05";done
+for i in {1..5};do make runSerial ARGS="7360 7360 0 73600 0 73600 0.25";done
+for i in {1..5};do make runSerial ARGS="7360 7360 0 73600 0 73600 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7430 7430 0 74300 0 74300 0.05";done
+for i in {1..5};do make runSerial ARGS="7430 7430 0 74300 0 74300 0.25";done
+for i in {1..5};do make runSerial ARGS="7430 7430 0 74300 0 74300 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7500 7500 0 75000 0 75000 0.05";done
+for i in {1..5};do make runSerial ARGS="7500 7500 0 75000 0 75000 0.25";done
+for i in {1..5};do make runSerial ARGS="7500 7500 0 75000 0 75000 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7570 7570 0 75700 0 75700 0.05";done
+for i in {1..5};do make runSerial ARGS="7570 7570 0 75700 0 75700 0.25";done
+for i in {1..5};do make runSerial ARGS="7570 7570 0 75700 0 75700 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7640 7640 0 76400 0 76400 0.05";done
+for i in {1..5};do make runSerial ARGS="7640 7640 0 76400 0 76400 0.25";done
+for i in {1..5};do make runSerial ARGS="7640 7640 0 76400 0 76400 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7710 7710 0 77100 0 77100 0.05";done
+for i in {1..5};do make runSerial ARGS="7710 7710 0 77100 0 77100 0.25";done
+for i in {1..5};do make runSerial ARGS="7710 7710 0 77100 0 77100 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7780 7780 0 77800 0 77800 0.05";done
+for i in {1..5};do make runSerial ARGS="7780 7780 0 77800 0 77800 0.25";done
+for i in {1..5};do make runSerial ARGS="7780 7780 0 77800 0 77800 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7850 7850 0 785000 0 785000 0.05";done
+for i in {1..5};do make runSerial ARGS="7850 7850 0 785000 0 785000 0.25";done
+for i in {1..5};do make runSerial ARGS="7850 7850 0 785000 0 785000 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7920 7920 0 79200 0 79200 0.05";done
+for i in {1..5};do make runSerial ARGS="7920 7920 0 79200 0 79200 0.25";done
+for i in {1..5};do make runSerial ARGS="7920 7920 0 79200 0 79200 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="7990 7990 0 79900 0 79900 0.05";done
+for i in {1..5};do make runSerial ARGS="7990 7990 0 79900 0 79900 0.25";done
+for i in {1..5};do make runSerial ARGS="7990 7990 0 79900 0 79900 0.5";done
+
+
+for i in {1..5};do make runSerial ARGS="8060 8060 0 80600 0 80600 0.05";done
+for i in {1..5};do make runSerial ARGS="8060 8060 0 80600 0 80600 0.25";done
+for i in {1..5};do make runSerial ARGS="8060 8060 0 80600 0 80600 0.5";done
